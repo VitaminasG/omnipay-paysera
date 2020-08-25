4296f1a0ae1fe386def830649833d65a53606d42
Prophe1 <ssnieskiokas@gmail.com>
2020-08-26 01:55:00 +0300

    Redirect view

--- src/Message/AcceptNotificationResponse.php
+++ src/Message/AcceptNotificationResponse.php
@@ -6,6 +6,8 @@ use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RequestInterface;
 use Omnipay\Common\Message\NotificationInterface;
 use Omnipay\Common\Exception\InvalidResponseException;
+use Symfony\Component\HttpFoundation\RedirectResponse as HttpRedirectResponse;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 class AcceptNotificationResponse extends AbstractResponse implements NotificationInterface
 {
@@ -96,6 +98,36 @@ class AcceptNotificationResponse extends AbstractResponse implements Notificatio
     public function getCode()
     {
         return $this->getDataValueOrNull('status');
+    }
+
+    /**
+     * @return HttpRedirectResponse|HttpResponse
+     */
+    public function getRedirectResponse()
+    {
+        $this->validateRedirect();
+
+        if ('GET' === $this->getRedirectMethod()) {
+            return HttpRedirectResponse::create($this->getRedirectUrl());
+        }
+
+        $hiddenFields = '';
+        foreach ($this->getRedirectData() as $key => $value) {
+            $hiddenFields .= sprintf(
+                    '<input type="hidden" name="%1$s" value="%2$s" />',
+                    htmlentities($key, ENT_QUOTES, 'UTF-8', false),
+                    htmlentities($value, ENT_QUOTES, 'UTF-8', false)
+                )."\n";
+        }
+
+        $output = view('payments.redirect')->render();
+        $output = sprintf(
+            $output,
+            htmlentities($this->getRedirectUrl(), ENT_QUOTES, 'UTF-8', false),
+            $hiddenFields
+        );
+
+        return HttpResponse::create($output);
     }
 
     /**
