c9cb82d08543d7884540d66a2c16ce762698ba05
Prophe1 <ssnieskiokas@gmail.com>
2020-08-26 02:01:25 +0300

    Fix

--- src/Message/AcceptNotificationResponse.php
+++ src/Message/AcceptNotificationResponse.php
@@ -101,36 +101,6 @@ class AcceptNotificationResponse extends AbstractResponse implements Notificatio
     }
 
     /**
-     * @return HttpRedirectResponse|HttpResponse
-     */
-    public function getRedirectResponse()
-    {
-        $this->validateRedirect();
-
-        if ('GET' === $this->getRedirectMethod()) {
-            return HttpRedirectResponse::create($this->getRedirectUrl());
-        }
-
-        $hiddenFields = '';
-        foreach ($this->getRedirectData() as $key => $value) {
-            $hiddenFields .= sprintf(
-                    '<input type="hidden" name="%1$s" value="%2$s" />',
-                    htmlentities($key, ENT_QUOTES, 'UTF-8', false),
-                    htmlentities($value, ENT_QUOTES, 'UTF-8', false)
-                )."\n";
-        }
-
-        $output = view('payments.redirect')->render();
-        $output = sprintf(
-            $output,
-            htmlentities($this->getRedirectUrl(), ENT_QUOTES, 'UTF-8', false),
-            $hiddenFields
-        );
-
-        return HttpResponse::create($output);
-    }
-
-    /**
      * Return the value from data or null.
      *
      * @param  string  $name
